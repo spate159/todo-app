4b6f0b7ab83dfd7038c6644df646b85e5ced72a2
Shyam Patel <spate159@hawk.iit.edu>
2017-09-12 22:12:57 -0500

    Add Priority feature and edit button

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -18,7 +18,9 @@ var app = app || {};
 		// The DOM events specific to an item.
 		events: {
 			'click .toggle': 'toggleCompleted',
+			'click .priority-btn': 'togglePriority',
 			'dblclick label': 'edit',
+			'click .edit-btn':'edit',
 			'click .destroy': 'clear',
 			'keypress .edit': 'updateOnEnter',
 			'keydown .edit': 'revertOnEscape',
@@ -50,6 +52,7 @@ var app = app || {};
 
 			this.$el.html(this.template(this.model.toJSON()));
 			this.$el.toggleClass('completed', this.model.get('completed'));
+			this.$el.toggleClass('priority', this.model.get('priority'));
 			this.toggleVisible();
 			this.$input = this.$('.edit');
 			return this;
@@ -63,6 +66,11 @@ var app = app || {};
 			return this.model.get('completed') ?
 				app.TodoFilter === 'active' :
 				app.TodoFilter === 'completed';
+		},
+
+		// Toggle the `"prioirty"` state of the model.
+		togglePriority: function() {
+			this.model.togglePriority();
 		},
 
 		// Toggle the `"completed"` state of the model.
